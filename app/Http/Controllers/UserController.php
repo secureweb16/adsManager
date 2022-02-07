acd86e51c0a9f8923eff77faa9629d2a93f808a8
secureweb16 <secureweb16@gmail.com>
2022-02-07 13:27:17 +0530

    Test Commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -471,34 +471,34 @@ fwrite($file, print_r($utmf,true));
 	}
 
 	public function curlapiCall($url,$data){
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => $url,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => "",
-        CURLOPT_TIMEOUT => 30000,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "POST",
-        CURLOPT_POSTFIELDS => json_encode($data),
-        CURLOPT_HTTPHEADER => array(
-            // Set here requred headers
-            "accept: */*",
-            "accept-language: en-US,en;q=0.8",
-            "content-type: application/json",
-        ),
-    ));
+		$curl = curl_init();
+		curl_setopt_array($curl, array(
+			CURLOPT_URL => $url,
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_ENCODING => "",
+			CURLOPT_TIMEOUT => 30000,
+			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+			CURLOPT_CUSTOMREQUEST => "POST",
+			CURLOPT_POSTFIELDS => json_encode($data),
+			CURLOPT_HTTPHEADER => array(
+				// Set here requred headers
+				"accept: */*",
+				"accept-language: en-US,en;q=0.8",
+				"content-type: application/json",
+			),
+		));
 
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-    curl_close($curl);
-    if ($err) {
-     return   json_decode(json_encode(array('status'=>0,'error'=>$err)));
-    } else {
-       return json_decode($response);
-    }
-  }
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+		curl_close($curl);
+		if ($err) {
+		 return   json_decode(json_encode(array('status'=>0,'error'=>$err)));
+		} else {
+		   return json_decode($response);
+		}
+	}
 
-  public function loginfromMarketplace(Request $request){
+	public function loginfromMarketplace(Request $request){
 		$request->validate([
 			'email' => 'required',
 			'uniquekey' => 'required',
